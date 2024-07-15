9f459fdd42d45923c21f1d98ce968ad096493548
Toivo Snåre <topisn@gmail.com>
2024-07-15 19:41:57 +0300

    update to zig master

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const module = b.addModule("sbi", .{
-        .root_source_file = .{ .path = "sbi.zig" },
+        .root_source_file = b.path("sbi.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -20,7 +20,7 @@ fn buildTests(b: *std.Build, optimize: std.builtin.OptimizeMode) void {
     const target_64 = b.resolveTargetQuery(.{ .cpu_arch = .riscv64, .os_tag = .freestanding });
     const build_test_64 = b.addStaticLibrary(.{
         .name = "test_64",
-        .root_source_file = .{ .path = "sbi.zig" },
+        .root_source_file = b.path("sbi.zig"),
         .target = target_64,
         .optimize = optimize,
     });
@@ -29,7 +29,7 @@ fn buildTests(b: *std.Build, optimize: std.builtin.OptimizeMode) void {
     const target_32 = b.resolveTargetQuery(.{ .cpu_arch = .riscv32, .os_tag = .freestanding });
     const build_test_32 = b.addStaticLibrary(.{
         .name = "test_32",
-        .root_source_file = .{ .path = "sbi.zig" },
+        .root_source_file = b.path("sbi.zig"),
         .target = target_32,
         .optimize = optimize,
     });
